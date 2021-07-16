5c83be5a2c53381513da16a845dba782079a38dc
MNosse <mateusnosse97@gmail.com>
2021-07-16 16:16:16 -0300

    mobileFinish

--- nlw/main.js
+++ nlw/main.js
@@ -16,16 +16,15 @@ for (const link of links) {
 }
 
 // mudar header da página quando houver scroll
-
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
   if (window.scrollY > navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 // Testimonials carousel
 const swiper = new Swiper('.swiper-container', {
@@ -50,6 +49,23 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links`,
+  #contact .text, #contact .links,
+  footer .brand, footer .social`,
   { interval: 100 }
 )
+
+// Botão voltar para o topo
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// When Scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
