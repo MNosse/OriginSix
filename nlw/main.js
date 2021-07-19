e7da356ec5ba34b4d3027a5be258bc32d4f7bcd1
MNosse <mateusnosse97@gmail.com>
2021-07-19 13:51:23 -0300

    final

--- nlw/main.js
+++ nlw/main.js
@@ -16,9 +16,9 @@ for (const link of links) {
 }
 
 // mudar header da página quando houver scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
   if (window.scrollY > navHeight) {
     header.classList.add('scroll')
   } else {
@@ -61,8 +61,8 @@ scrollReveal.reveal(
 )
 
 // Botão voltar para o topo
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -70,8 +70,34 @@ function backToTop() {
   }
 }
 
+// Menu ativo conforme a seção visivel na página
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 // When Scroll
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
